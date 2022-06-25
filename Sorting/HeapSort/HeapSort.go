e701d4a2d893712e28869bb9faf74c1c4250a870
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-06-25 07:12:43 +0200

    refactor⚙️(HeapSort): Add struct for MaxHeap

--- Sorting/HeapSort/HeapSort.go
+++ Sorting/HeapSort/HeapSort.go
@@ -1,46 +1,12 @@
 package MergeSort
 
 func HeapSort(slice []int) []int {
-	heap := buildHeap(slice)
+	heap := NewMaxHeap(slice)
+	heap.BuildHeap()
 
 	for length := len(slice); length > 1; length-- {
-		slice = removeTop(heap, length)
+		heap.RemoveTop(length)
 	}
 
-	return heap
-}
-
-func removeTop(heap []int, length int) []int {
-	lastIndex := length - 1
-	heap[0], heap[lastIndex] = heap[lastIndex], heap[0]
-	return heapify(heap, 0, lastIndex)
-}
-
-func buildHeap(slice []int) []int {
-	for i := len(slice) / 2; i >= 0; i-- {
-		heapify(slice, i, len(slice))
-	}
-
-	return slice
-}
-
-func heapify(slice []int, rootIdx int, length int) []int {
-	var max = rootIdx
-	l := (rootIdx * 2) + 1
-	r := (rootIdx * 2) + 2
-
-	if l < length && slice[l] > slice[max] {
-		max = l
-	}
-
-	if r < length && slice[r] > slice[max] {
-		max = r
-	}
-
-	if max != rootIdx {
-		slice[rootIdx], slice[max] = slice[max], slice[rootIdx]
-		slice = heapify(slice, max, length)
-	}
-
-	return slice
+	return heap.heap
 }
---
+++ Sorting/HeapSort/MaxHeap.go
@@ -0,0 +1,41 @@
+package MergeSort
+
+type MaxHeap struct {
+	heap []int
+}
+
+func NewMaxHeap(slice []int) *MaxHeap {
+	copySlice := make([]int, len(slice))
+	copy(copySlice, slice)
+	return &MaxHeap{copySlice}
+}
+
+func (h *MaxHeap) BuildHeap() {
+	for i := len(h.heap) / 2; i >= 0; i-- {
+		h.Heapify(i, len(h.heap))
+	}
+}
+
+func (h *MaxHeap) Heapify(rootIdx, length int) {
+	var max = rootIdx
+	l := (rootIdx * 2) + 1
+	r := (rootIdx * 2) + 2
+
+	if l < length && h.heap[l] > h.heap[max] {
+		max = l
+	}
+	if r < length && h.heap[r] > h.heap[max] {
+		max = r
+	}
+
+	if max != rootIdx {
+		h.heap[rootIdx], h.heap[max] = h.heap[max], h.heap[rootIdx]
+		h.Heapify(max, length)
+	}
+}
+
+func (h *MaxHeap) RemoveTop(length int) {
+	lastIndex := length - 1
+	h.heap[0], h.heap[lastIndex] = h.heap[lastIndex], h.heap[0]
+	h.Heapify(0, lastIndex)
+}
