c4e90263e12377e359d479ead70ef2dd7758a005
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-06-26 08:17:59 +0200

    feat✨(BinarySearch)

---
+++ Searching/BinarySearch/BinarySearch.go
@@ -0,0 +1,24 @@
+package binarySearch
+
+func BinarySearch(array []int, target int) int {
+	return searchInArray(array, 0, len(array)-1, target)
+}
+
+func searchInArray(array []int, start int, end int, target int) int {
+	if start > end ||
+		len(array) == 0 {
+		return -1
+	}
+
+	keyIdx := (end + start) / 2
+	keyValue := array[keyIdx]
+
+	if keyValue == target {
+		return keyIdx
+	} else if target > keyValue {
+		return searchInArray(array, keyIdx+1, end, target)
+	} else {
+		return searchInArray(array, start, keyIdx-1, target)
+	}
+
+}
